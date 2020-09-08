2777e94f89c22c1c4ccebbe9951ddc23d8aba019
sram <sram@ethz.ch>
2020-09-08 16:11:54 +0200

    added a number of stuffs

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,10 +1,1 @@
-(function() {
-  if (location.href.indexOf("http://localhost:4000/mysite/contact/#email_successfully_sent") === 0) {
-    $("#email_successfully_sent").modal("show");
-    $('#email_successfully_sent').on('hidden.bs.modal', function() {
-      document.location.href = String(document.location.href).replace('#email_successfully_sent', '');
-      return false;
-    });
-  }
-
-}).call(this);
+(function(){0===location.href.indexOf("https://sumitram.github.io/mysite/contact/#email_successfully_sent")&&($("#email_successfully_sent").modal("show"),$("#email_successfully_sent").on("hidden.bs.modal",function(){return document.location.href=String(document.location.href).replace("#email_successfully_sent",""),!1}))}).call(this);
